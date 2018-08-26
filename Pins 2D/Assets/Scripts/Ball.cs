2e3ed9acbfe96747a66d6b052f9a9d0996c5c929
claudiom4sir <claudio_rota@hotmail.it>
2018-08-26 02:58:57 +0200

    first session done
    added basic elements and script.
    next work: pinsspawn script

---
+++ Pins 2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] float rotationSpeed = 20f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Pins 2D/Assets/Scripts/Pin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float speed = 5f;
+    bool targetReached = false;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        if (targetReached)
+            return;
+        rb.MovePosition(rb.position + Vector2.up * speed * Time.fixedDeltaTime);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) // when pin touchs something
+    {
+        if(other.tag == "Ball")
+        {
+            transform.SetParent(target);
+            targetReached = true;
+            enabled = false;
+        }
+        
+    }
+
+}
